96a30d10bb338195dca20fa7961fe646b7b65fb0
Taha <taha.hussein.two@gmail.com>
2023-04-01 00:06:05 +0200

    add some helper files

---
+++ Pizza  App Demo/Extension/UIColor+Extensions.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+Extensions.swift
+//  PizzaWorld
+//
+//  Created by Osama on 10/15/20.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIColor {
+  @nonobjc class var pinkishRed: UIColor {
+    return UIColor(red: 229.0 / 255.0, green: 41.0 / 255.0, blue: 62.0 / 255.0, alpha: 1.0)
+  }
+    
+    @nonobjc class var darkBlueGrey: UIColor {
+      return UIColor(red: 11.0 / 255.0, green: 32.0 / 255.0, blue: 49.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var pinkishRedTwo: UIColor {
+      return UIColor(red: 228.0 / 255.0, green: 40.0 / 255.0, blue: 61.0 / 255.0, alpha: 1.0)
+    }
+
+}
